ddaac16c3a50cb77d1a2ff2b6e590e51fa9417c4
Gilad Kariv <giladkariv@mac.local>
2025-06-13 23:18:38 +0300

    fix: add alembic migrations initialization to build process

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+from .base import Base, BaseModel
+from .user import User
+from .task import Task
+from .project import Project
+from .comment import Comment
+from .notification import Notification 
